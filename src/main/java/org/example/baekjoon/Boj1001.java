1a884f377099cea06a2887cab797ccaaa33dedc1
mikekang47 <gyeong0308@gmail.com>
2022-12-27 11:38:20 +0900

    Solve: boj1001

---
+++ src/main/java/org/example/baekjoon/Boj1001.java
@@ -0,0 +1,12 @@
+package org.example.baekjoon;
+
+import java.util.Scanner;
+
+public class Boj1001 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a - b);
+	}
+}
